724f489bb0f26d2058aa709593451ad485d3d58f
Mattias Petter Johansson <mpj@mpj.me>
2012-07-02 00:19:56 +0200

    Fixed date generation bug

--- routes/index.js
+++ routes/index.js
@@ -127,7 +127,11 @@ function currentDateFitbitString() {
   var now = new Date();
   var month = (now.getUTCMonth()+1).toString();
   if (month.length == 1) month = "0" + month;
-  return now.getUTCFullYear() + '-' +  month + '-' + now.getUTCDate();
+
+  var date = now.getUTCDate().toString();
+  if (date.length == 1) date = "0" + date;
+
+  return now.getUTCFullYear() + '-' +  month + '-' + date;
 }
 
 function getDevices(token, cb) {
